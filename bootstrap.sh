1e273a842bf5d53012c15d5c159aa8bffc83140a
spacecadet9 <danielhpavey@gmail.com>
2014-11-10 12:23:01 +0000

    Updated bootstap - php now works...

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get install -y apache2
-apt-get install php5
-apt-get install php5-mysql
+apt-get install -y apache2 php5 php5-mysql libapache2-mod-php5
 rm -rf /var/www
 ln -fs /vagrant /var/www
