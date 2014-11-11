735f7874479d6dc2427b7ba253e3a689f44f5bde
spacecadet9 <danielhpavey@gmail.com>
2014-11-11 11:57:33 +0000

    Updated vagrant file and provisioning script so that mod_rewrite works...

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,3 +4,26 @@ apt-get update
 apt-get install -y apache2 php5 php5-mysql libapache2-mod-php5
 rm -rf /var/www
 ln -fs /vagrant /var/www
+
+# Add ServerName to httpd.conf
+echo "ServerName localhost" > /etc/apache2/httpd.conf
+# Setup hosts file
+VHOST=$(cat <<EOF
+<VirtualHost *:80>
+  DocumentRoot "/vagrant/"
+  ServerName localhost
+  <Directory "/vagrant/">
+    Options Indexes FollowSymLinks MultiViews
+    AllowOverride All
+    Order allow,deny
+    Allow from all
+  </Directory>
+</VirtualHost>
+EOF
+)
+echo "${VHOST}" > /etc/apache2/sites-enabled/000-default
+
+# Enable mod_rewrite
+a2enmod rewrite
+# Restart apache
+service apache2 restart
