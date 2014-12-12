d52f59de520fe93a3dc603890a036411f889fcb9
spacecadet9 <danielhpavey@gmail.com>
2014-12-12 16:17:01 +0000

    Added php5-gd to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sudo apt-get update
-sudo apt-get install -y apache2 php5 php5-mysql libapache2-mod-php5
+sudo apt-get install -y apache2 php5 php5-mysql libapache2-mod-php5 php5-gd
 sudo rm -rf /var/www
 sudo ln -fs /vagrant /var/www
 # Add ServerName to httpd.conf
